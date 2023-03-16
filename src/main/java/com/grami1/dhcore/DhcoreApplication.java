e203be789b5478d5362209145940443504675691
grami1 <swdevnode@gmail.com>
2023-03-16 21:36:03 +0100

    initial commit

---
+++ src/main/java/com/grami1/dhcore/DhcoreApplication.java
@@ -0,0 +1,13 @@
+package com.grami1.dhcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhcoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhcoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grami1/dhcore/DhcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grami1.dhcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
